8d570c94b9c8e6e44cd766f212391ad2bffd276c
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-24 15:46:30 +0530

    Added Start Timer functionality which will decrease the Time to 00:00:00

---
+++ src/01-timer.js
@@ -0,0 +1,34 @@
+let divRoot = document.getElementById("root");
+
+let hours = 0;
+let minutes = 0;
+let seconds = 10;
+
+let text = document.getElementById("time-heading");
+let t;
+let status = false;
+
+startTimer = (event) => {
+  if (status == false)
+    t = setInterval(() => {
+      status = true;
+      if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+        if (minutes < 0) {
+          minutes = 59;
+          hours--;
+          if (hours < 0) {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+            document.getElementById("time-heading").innerHTML =
+              "00:00:00 Timer Expired";
+          }
+        }
+      }
+      if (text.innerHTML !== "00:00:00 Timer Expired")
+        text.innerHTML = hours + ":" + minutes + ":" + seconds;
+      seconds--;
+    }, 1000);
+};
