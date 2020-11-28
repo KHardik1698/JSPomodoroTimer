353770180f8fdcce4b71ff1f685f3f0c849b0479
KHardik1698 <hardik.kundalia@contentstack.com>
2020-11-28 14:46:20 +0530

    Added Input Validation for Pomodoro & Short Break Timers
    The input validation will activate an alert when the Input is invalid

--- src/01-timer.js
+++ src/01-timer.js
@@ -11,24 +11,28 @@ let displaySeconds = seconds;
 
 pomodoro = (event) => {
   let pomoInput = document.getElementById("pomoDefault").value;
-  if (pomoInput) {
+  let pomoFilter = inputValidation(pomoInput, 60);
+  if (pomoFilter) {
     minutes = pomoInput;
   } else {
     minutes = 25;
   }
   seconds = 0;
+  document.getElementById("pomoDefault").value = "";
   document.getElementById("current-timer-heading").innerHTML =
     "Current Timer : Pomodoro";
 };
 
 shortBreak = (event) => {
   let shortInput = document.getElementById("shortDefault").value;
-  if (shortInput) {
+  let shortFilter = inputValidation(shortInput, 15);
+  if (shortFilter) {
     minutes = shortInput;
   } else {
     minutes = 5;
   }
   seconds = 0;
+  document.getElementById("shortDefault").value = "";
   document.getElementById("current-timer-heading").innerHTML =
     "Current Timer : Short Break";
 };
@@ -125,4 +129,31 @@ resetTimer = (event) => {
     } Seconds`
   );
   elapsed = 0;
+};
+
+const inputValidation = (input, limit) => {
+  str = input;
+  num = parseInt(input);
+  return isValid(num, str, limit);
+};
+
+const isValid = (num, str, limit) => {
+  if (parseInt(str) !== num && str.length > 0) {
+    window.alert("Enter a number");
+    return false;
+  } else if (str.length > 2) {
+    window.alert("Enter a 2 Digit Number");
+    return false;
+  } else if (num < 0 || num > 60) {
+    window.alert("Enter a number between 1 to 60");
+    return false;
+  } else if (parseInt(str) == num && str.length !== 1 && str.length > 2) {
+    window.alert("Enter without Blank Spaces");
+  } else if (str.length == 0) {
+    return false;
+  } else if (num > limit) {
+    window.alert(`Enter a number less than ${limit}`);
+  } else {
+    return true;
+  }
 };
